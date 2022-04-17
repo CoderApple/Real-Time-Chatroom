92a5ceb46c4636ba712bb2b7eab7b0314d48ea3d
Akshat Singh <95408545+CoderApple@users.noreply.github.com>
2022-04-17 12:58:03 +0530

    removing dateFns

--- scripts/ui.js
+++ scripts/ui.js
@@ -9,15 +9,14 @@ class ChatUI{
         this.list.innerHTML = '';
     }
     render(data){
-        const when = dateFns.distanceInWordsToNow(
-            data.created_at.toDate(),
-            {addSuffix: true}
-        );
+//         const when = dateFns.distanceInWordsToNow(
+//             data.created_at.toDate(),
+//             {addSuffix: true}
+//         );
         const html = `
             <li class= "list-group-item">
                 <span class="username">${data.username}</span>
                 <span class="message">${data.message}</span>
-                <div class="time">${when}</div>
             </li>
         `;
 
